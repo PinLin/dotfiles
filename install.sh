bb8505842eb1ba7814115f0e49a9e5e9fc091b36
PinLin <moneycat711@gmail.com>
2017-06-29 00:24:57 +0800

    fix auto-suggest plugin install fail

--- install.sh
+++ install.sh
@@ -53,9 +53,9 @@ if ! [ -f ~/.zshrc.bak ]; then
 fi
 echo "source ~/$repo/zsh/sample.zshrc" > ~/.zshrc
 # install zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # pause
 if ! command -v pause 1>/dev/null; then
