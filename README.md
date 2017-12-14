e2e17cd732a9a7e4443ed0ea7be4c0b1a20a9187
Pin Lin <moneycat711@gmail.com>
2017-12-14 18:45:25 +0800

    change installation

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
 # .shconf
 
-This is my shell configuration.
+This is my shell config.
 
 ## Requirements
 
-Use **Package manager** to install:`wget`, `git`, `vim`, `zsh`.
++ `git`: a powerful version control tool.
 
-And There is a **Install Script** of [`oh-my-zsh`](http://ohmyz.sh):
++ `vim `: a useful text editor on terminal.
+
++ `zsh`: a fashion command line shell.
+
++ [`oh-my-zsh`](http://ohmyz.sh): a amazing `zsh` framework.
+
+```sh
+# Install by wget
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install by curl
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+```
 
 ## Install
 
 ```sh
+# Install by wget
 wget -qO- https://raw.githubusercontent.com/PinLin/.shconf/master/install.sh | bash
+
+# Install by curl
+curl -o- https://raw.githubusercontent.com/PinLin/.shconf/master/install.sh | bash
 ```
-You need to install the font-type which in `font` directory.
+
+ The special font-type that Powerline may need was put in the `font` directory.
--- install.sh
+++ install.sh
@@ -1,28 +1,21 @@
 # set constant
 repo='.shconf'
 
-# check reqiurements
-fail=0
+# check reqirements
+# apps
 for app in "git" "vim" "zsh"; do
-    if ! command -v $app 1>/dev/null; then 
-        if [ $fail == 0 ]; then 
-            echo 
-            echo -n 'Please install'
-            fail=1
-        else
-            echo -n ','
-        fi
-        echo -n ' `'$app'`'
+    if ! command -v $app >/dev/null; then
+        echo 'Could not find `'$app'`.'
+        exit 1
     fi
 done
-if [ $fail == 1 ]; then
-    echo ' before run this script.'
-    exit 1
-fi
-if ! [ -d ~/.oh-my-zsh ]; then
-    echo 'You should install `oh-my-zsh` before run this script.'
-    exit 1
-fi
+# files
+for app in "~/.oh-my-zsh"; do
+    if ! [ -d $app ]; then
+        echo 'Could not find `'$app'`.'
+        exit 1
+    fi
+done
 
 # clone into computer
 if [ -d ~/$repo ]; then
@@ -30,13 +23,13 @@ if [ -d ~/$repo ]; then
 fi
 git clone https://github.com/PinLin/$repo ~/$repo
 if [ $? != 0 ]; then
-    echo 
-    echo 'Cloning Error, maybe you should check your network connection.'
+    echo ''
     exit 2
 fi
 cd ~/$repo
 
 # setup vim
+# backup old config
 if ! [ -f ~/.vimrc.bak ]; then
     if [ -f ~/.vimrc ]; then 
         mv ~/.vimrc ~/.vimrc.bak 
@@ -45,7 +38,7 @@ fi
 echo "source ~/$repo/vim/sample.vimrc" > ~/.vimrc
 
 # setup zsh
-# backup old configure
+# backup old config
 if ! [ -f ~/.zshrc.bak ]; then
     if [ -f ~/.zshrc ]; then 
         mv ~/.zshrc ~/.zshrc.bak 
@@ -56,9 +49,3 @@ echo "source ~/$repo/zsh/sample.zshrc" > ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# pause
-if ! command -v pause 1>/dev/null; then
-    alias pause="$HOME/.pause/bin/pause"
-    wget -qO- https://raw.githubusercontent.com/PinLin/EHG/master/pause/install.sh | bash
-fi
