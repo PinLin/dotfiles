56b0a67a33a442ad11b75e8834df1144858071c1
Pin Lin <pinlin945@gmail.com>
2022-03-23 04:43:49 +0800

    Correct FOREGROUND of os identifier

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -186,7 +186,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=0
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
